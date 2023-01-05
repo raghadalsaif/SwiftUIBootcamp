54dd375bd44201fa86ecf6f3ee9e09d231704c4e
raghad <raghad.k.alsaif@gmail.com>
2023-01-05 18:12:33 +0300

    Initial Commit

---
+++ Shaps#3/Shaps_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Shaps_3App.swift
+//  Shaps#3
+//
+//  Created by raghad khalid alsaif on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Shaps_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
